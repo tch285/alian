3d5c527131b4b73e1a54afb3e04f3ddff0c8336d
matplo <ploskon@gmail.com>
2024-11-22 23:41:00 -0800

    alian path w/o calling env - test first

--- alian/__init__.py
+++ alian/__init__.py
@@ -7,8 +7,15 @@ class AliAnSettings(GenericObject):
 	def __init__(self, **kwargs):
 		super(AliAnSettings, self).__init__(**kwargs)
 
-
-alian_settings = AliAnSettings(src_path=os.path.dirname(os.path.abspath(__file__)), path=os.environ['ALIAN_DIR'])
+def get_alian_path():
+  spath = os.path.dirname(os.path.abspath(__file__)).rstrip('/lib/alian')
+  try:
+    spath = os.environ['ALIAN_DIR']
+  except:
+    pass
+  return spath
+    
+alian_settings = AliAnSettings(src_path=os.path.dirname(os.path.abspath(__file__)), path=get_alian_path())
 
 # here load all the ${ALIAN_DIR}/include/*.hh files with cppyy and the libraries found under ..${ALIAN_DIR}/lib
 # use cppyyhelper to do this
