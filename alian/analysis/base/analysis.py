5776b0c36c7c80785ae4e1c790126d7f57507791
matplo <ploskon@gmail.com>
2024-11-30 00:04:25 -0800

    + constit subtr

---
+++ alian/analysis/base/analysis.py
@@ -0,0 +1,10 @@
+import heppyy 
+
+class BaseAnalysis(heppyy.GenericObject):
+    _defaults = {}
+
+    def __init__(self, **kwargs):
+        super(BaseAnalysis, self).__init__(**kwargs)
+        for k, val in self.__class__._defaults.items():
+            if not hasattr(self, k) or getattr(self, k) is None:
+                setattr(self, k, val)
