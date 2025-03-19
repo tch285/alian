3c60ed4ea051f34a65b7d12f61faf169236507bf
matplo <ploskon@gmail.com>
2025-03-19 11:41:22 +0100

    more trees

--- alian/sandbox/charm/pythia_charm.py
+++ alian/sandbox/charm/pythia_charm.py
@@ -38,6 +38,7 @@ def main():
 	parser.add_argument('--ncorrel', help='max n correlator', type=int, default=2)
 	parser.add_argument('-o','--output', help='root output filename', default='pythia_charm.root', type=str)
 	parser.add_argument('--jet-pt-min', help='jet pt min', default=10.0, type=float)
+	parser.add_argument('--charm-pt-min', help='charm pt min', default=5.0, type=float)
 	args = parser.parse_args()
 
 	pythia = Pythia8.Pythia()
@@ -57,13 +58,14 @@ def main():
 	# output
 	fout = SingleRootFile(args.output)
 	fout.root_file.cd()
-	tn_events = ROOT.TNtuple("tn_events", "tn_events", "sigma:weight:code")
-	tn = ROOT.TNtuple("tn", "tn", "sigma:weight:code:pt:eta:phi:mass:R:leadpid")
+	tn_events = ROOT.TNtuple("tn_events", "tn_events", "sigma:weight:code:out1pid:out2pid")
+	tn_jets = ROOT.TNtuple("tn_jets", "tn_jets", "sigma:weight:code:pt:eta:phi:mass:R:leadpid:has_charm")
 	tn_partons = ROOT.TNtuple("tn_partons", "tn_partons", "sigma:weight:code:pid:pt:eta:phi:mass")
-	fout.add(tn)
  
 	# configure pythia
-	mycfg = ['HadronLevel:all = off', f'PhaseSpace:pThatMin = {args.jet_pt_min}']
+	mycfg = ['HadronLevel:all = off', 
+          f'PhaseSpace:pThatMin = {args.jet_pt_min}',
+          'PhaseSpace:bias2Selection = off']
 	pythia = pyconf.create_and_init_pythia_from_args(args, mycfg)
 	if not pythia:
 		print("[e] pythia initialization failed.")
@@ -72,11 +74,17 @@ def main():
 	# event loop
 	if args.nev < 100:
 		args.nev = 100
-	for i in tqdm.tqdm(range(args.nev)):
+	accepted = 0
+	pbar = tqdm.tqdm(total=args.nev)
+	while accepted < args.nev:
 		if not pythia.next():
 			continue
 		abs_fs_part_codes = [abs(p.id()) for p in pythia.event if p.isFinal()]
 		if 4 not in abs_fs_part_codes:
+			continue
+		# charm above a pt
+		charm_above_pt = [p for p in pythia.event if p.isFinal() and abs(p.id()) == 4 and p.pT() > args.jet_pt_min and abs(p.eta()) < 1]
+		if len(charm_above_pt) == 0:
 			continue
 		# parts = vector[fj.PseudoJet]([fj.PseudoJet(p.px(), p.py(), p.pz(), p.e()) for p in pythia.event if p.isFinal() and p.isCharged()])
 		# parts = pythiafjext.vectorize(pythia, True, -1, 1, False)
@@ -84,7 +92,7 @@ def main():
 		sigma = pythia_info.sigmaGen()
 		weight = pythia.info.weight()
 		leading_process_code = pythia_info.code()
-		tn_events.Fill(sigma, weight, leading_process_code)
+		tn_events.Fill(sigma, weight, leading_process_code, pythia.event[4].id(), pythia.event[5].id())
 		_ = [tn_partons.Fill(sigma, weight, leading_process_code, p.id(), p.pT(), p.eta(), p.phi(), p.m()) for p in pythia.event if p.isFinal() and p.isParton()]
 
 		# jet finding
@@ -93,11 +101,15 @@ def main():
 			jet_def = jet_defs[R]
 			jet_selector = jet_selectors[R]
 			jets = jet_selector(jet_def(parts))
+			if R==0.4 and len(jets) > 0:
+				accepted += 1
+				pbar.update(1)
 			for j in jets:
 				leading = fj.sorted_by_pt(j.constituents())[0]
 				leading_pythia_id = leading.user_index()
 				leading_pythia = pythia.event[leading_pythia_id]
-				tn.Fill(sigma, weight, leading_process_code, j.perp(), j.eta(), j.phi(), j.m(), R, leading_pythia.id())
+				has_charm = any([abs(pythia.event[p.user_index()].id()) == 4 for p in j.constituents()])
+				tn_jets.Fill(sigma, weight, leading_process_code, j.perp(), j.eta(), j.phi(), j.m(), R, leading_pythia.id(), int(has_charm))
 
 	pythia.stat()
 
@@ -107,3 +119,5 @@ def main():
 
 if __name__ == '__main__':
 	main()
+ 
+ 
