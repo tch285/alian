1fef7c4a08181b6b7dbbd3f0f59a2c14f2e98458
matplo <ploskon@gmail.com>
2025-03-26 20:57:57 +0000

    ecm

--- alian/sandbox/charm/run_parallel.sh
+++ alian/sandbox/charm/run_parallel.sh
@@ -24,11 +24,7 @@ function make_job_string() {
     outfname=$3
     # replace .root with _seed.root
     outfname=${outfname/.root/_$seed.root}
-<<<<<<< HEAD
-    job_string="./pythia_charm.py --nev $nev --py-seed $seed -o $outfname > pythia_charm_$seed.log 2>&1"
-=======
     job_string="./pythia_charm.py --nev $nev --py-seed $seed -o $outfname --py-ecm 5020 > log_$seed.txt 2>&1"
->>>>>>> b5a8570 (charm tests with pythia)
     echo $job_string
 }
 export -f make_job_string
