9836b9727a745b36a6fedb7a4c18af643f96761d
Tucker Hwang <tucker_hwang@berkeley.edu>
2025-06-18 16:47:44 -0700

    Add on-the-fly Pythia

---
+++ alian/sandbox/cEEC/sherpa_otf_ceec.py
@@ -0,0 +1,264 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging
+import os
+import sys
+import time
+from itertools import permutations
+
+import numpy as np
+import ROOT
+import yaml
+
+import heppyy.util.fastjet_cppyy # noqa: F401
+# import heppyy.util.pythia8_cppyy
+# import heppyy.util.heppyy_cppyy
+
+from cppyy.gbl import fastjet as fj
+from cppyy.gbl.std import vector
+from heppyy.pythia_util import configuration as pyconf
+
+# to import more from heppyy: import the NAMESPACE declared in the .hh file
+# ex: for pythiafjext/pyfjtools.hh, we import:
+# from cppyy.gbl import pythiafjtools
+
+logger = logging.getLogger(__name__)
+
+def linbins(xmin, xmax, nbins):
+    return np.linspace(xmin, xmax, nbins + 1)
+
+def logbins(xmin, xmax, nbins):
+    return np.logspace(np.log10(xmin), np.log10(xmax), nbins + 1)
+
+def part_w_charge(pyp):
+    pj = fj.PseudoJet(pyp.px(), pyp.py(), pyp.pz(), pyp.e())
+    pj.set_user_index(int(pyp.charge()))
+    return pj
+
+class PythiaOTFENC(object):
+    def __init__(self, config, odir, ofn, nev, verbose):
+        self.config = config
+        self.verbose = verbose
+        self.output_path = f"{odir}/{ofn}"
+        if nev < 10:
+            logger.warning(f"Number of events ({nev}) too low, setting to 10.")
+            self.nev = 10
+        else:
+            self.nev = nev
+        self.nfailed = 0
+        self.parse_config()
+
+    def parse_config(self):
+        with open(self.config, "r") as stream:
+            config = yaml.safe_load(stream)
+        self.jetR = config["jetR"] if "jetR" in config else 0.4
+        self.eta_max = config["eta_max"]
+        self.min_part_trk_pT = config["min_part_trk_pT"]
+        self.thr = config["thr"]
+        self.min_jet_pT = config["min_jet_pT"]
+        self.RL_min, self.RL_max, self.RL_nbins = config["RL_binning"]
+        self.pT_min, self.pT_max, self.pT_nbins = config["pT_binning"]
+        self.shower = config["shower"]
+        if self.shower not in ["dire", "vincia", "simple"]:
+            logger.warning(
+                f"Shower model {self.shower} not recognized, defaulting to simple."
+            )
+            self.shower = "simple"
+        self.custom_tune = config["custom_tune"]
+
+    def prepare(self):
+        self.RL_bins = logbins(self.RL_min, self.RL_max, self.RL_nbins)
+        self.pT_bins = linbins(self.pT_min, self.pT_max, self.pT_nbins)
+
+        self.hists = {}
+        self.hists['evw'] = ROOT.TH1F("evw", "evw;evw;cts", 100, -3.0, 3.0)
+        self.hists['nev'] = ROOT.TH1F("hnev", "nev", 2, -0.5, 1.5)
+        
+        for ptype in ["T", "Q", "P", "M", "PM"]:
+            self.hists[ptype] = ROOT.TH2D(
+                f"E2C_{ptype}_{self.thr}",
+                f"E2C_{ptype}_{self.thr};jet pt;R_{{L}}",
+                self.pT_nbins,
+                self.pT_bins,
+                self.RL_nbins,
+                self.RL_bins,
+            )
+        self.hists["jet_pT"] = ROOT.TH1D(
+            "jet_pT", "jet pt;pt gev;cts", self.pT_nbins, self.pT_bins
+        )
+
+        self.jet_def = fj.JetDefinition(fj.antikt_algorithm, self.jetR)
+
+        self.jet_selector = fj.SelectorPtMin(self.min_jet_pT) * fj.SelectorAbsEtaMax(
+            self.eta_max - self.jetR
+        )
+
+        self.part_pT_selector = fj.SelectorPtMin(self.min_part_trk_pT)
+        self.thr_selector = fj.SelectorPtMin(self.thr)
+
+        self.user_config = ['Init:showProcesses = off', 'Init:showChangedParticleData = off']
+        if self.shower == "vincia":
+            self.user_config.append("PartonShowers:Model = 2")
+            if not self.custom_tune:
+                self.user_config.append("Vincia:Tune = -1")
+        elif self.shower == "dire":
+            self.user_config.append("PartonShowers:Model = 3")
+            if not self.custom_tune:
+                self.user_config.append("Dire:Tune = -1")
+
+    def generate(self, args):
+        self.prepare()
+
+        # pythia = Pythia8.Pythia()
+        fj.ClusterSequence.print_banner()
+
+        self.start = time.perf_counter()
+        pythia = pyconf.create_and_init_pythia_from_args(args, self.user_config)
+
+        if not pythia:
+            logger.fatal("Pythia initialization failed!")
+            sys.exit(1)
+        else:
+            logger.info("Pythia configured and initialized.")
+
+        self.simulate(pythia)
+        logger.info(f"Simulation step completed in {time.perf_counter() - self.start} sec.")
+
+        self.finalize(pythia)
+        logger.info(f"Histogram scaling/saving completed in {time.perf_counter() - self.start} sec.")
+
+    def simulate(self, pythia):
+        iev = 0
+        while iev < self.nev:
+            if iev % 100 == 0:
+                logger.debug(f"ievt: {iev}")
+
+            if not pythia.next():
+                self.nfailed += 1
+                continue
+
+            # parts = vector[fj.PseudoJet](
+            #     [
+            #         fj.PseudoJet(p.px(), p.py(), p.pz(), p.e())
+            #         for p in pythia.event
+            #         if p.isFinal() and p.isCharged()
+            #     ]
+            # )
+            parts_ch = vector[fj.PseudoJet](
+                [part_w_charge(p) for p in pythia.event if p.isFinal() and p.isCharged()]
+            )
+
+            self.evw = pythia.info.weight()
+            self.analyze_event(parts_ch)
+
+            # Some "accepted" events don't survive hadronization step -- keep track here
+            self.hists['nev'].Fill(0, self.evw)
+            self.hists['evw'].Fill(self.evw)
+            
+            # logger.info('NEW EVENT:')
+            # logger.info(f"Sum of weights (histogram): {self.hists['nev'].GetBinContent(1)}")
+            # logger.info(f"Sum of weights (Pythia): {pythia.info.weightSum()}")
+            iev += 1
+            # if iev == 10:
+            #     break
+
+    def analyze_event(self, parts):
+        jets = fj.sorted_by_pt(
+            self.jet_selector(self.jet_def(self.part_pT_selector(parts)))
+        )
+        for jet in jets:
+            self.analyze_jet(jet)
+
+    def analyze_jet(self, jet):
+        jet_pT = jet.perp()
+        self.hists["jet_pT"].Fill(jet_pT, self.evw)
+
+        constituents = self.thr_selector(jet.constituents())
+
+        for p1, p2 in permutations(constituents, 2):
+            q1 = p1.user_index()
+            q2 = p2.user_index()
+            w = p1.perp() * p2.perp() / jet_pT / jet_pT
+            rL = np.sqrt(p1.delta_phi_to(p2) ** 2 + (p1.eta() - p2.eta()) ** 2)
+            if q1 > 0 and q2 > 0:
+                ptype = "P"
+            elif q1 < 0 and q2 < 0:
+                ptype = "M"
+            else:
+                ptype = "PM"
+
+            self.hists[ptype].Fill(jet_pT, rL, w * self.evw)
+            self.hists["T"].Fill(jet_pT, rL, w * self.evw)
+            self.hists["Q"].Fill(jet_pT, rL, w * q1 * q2 * self.evw)
+
+    def finalize(self, pythia):
+        pythia.stat()
+        self.hists['nev'].SetBinError(1, 0)
+
+        scale_f = pythia.info.sigmaGen() / self.hists['nev'].GetBinContent(1)
+
+        logger.info(f"sigmaGen (Pythia) = {pythia.info.sigmaGen()} +- {pythia.info.sigmaErr()}")
+        logger.info(f"Scaling factor (sigma/nev) = {scale_f}")
+        logger.info(f"nTried: {pythia.info.nTried()}")
+        logger.info(f"nSelected: {pythia.info.nSelected()}")
+        logger.info(f"nAccepted: {pythia.info.nAccepted()}")
+        logger.info(f"Failed: {self.nfailed}")
+        logger.info(f"Sum of weights (histogram): {self.hists['nev'].GetBinContent(1)}")
+        logger.info(f"Sum of weights (Pythia): {pythia.info.weightSum()}")
+        logger.info(f"Number of successful events (histogram): {self.hists['nev'].GetEntries()}")
+        # with open(f"{self.output_dir}/scales.txt", 'w') as f:
+        #     f.write(f"{scale_f}")
+        self._scale_and_save_hists(scale_f)
+
+        logger.info(
+            f"N total final events: {self.hists['nev'].GetBinContent(1)} with {pythia.info.nAccepted() - self.hists['nev'].GetBinContent(1)} events rejected."
+        )
+
+    def _scale_and_save_hists(self, scale_f):
+        for ptype in ["T", "Q", "P", "M", "PM"]:
+            self.hists[ptype].Scale(scale_f)
+        self.hists["jet_pT"].Scale(scale_f)
+        
+        with ROOT.TFile(self.output_path, "RECREATE") as f:
+            for hist in self.hists.values():
+                f.WriteTObject(hist)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="pythia8 OTF + FJ for cEEC", prog=os.path.basename(__file__)
+    )
+    pyconf.add_standard_pythia_args(parser)
+    parser.add_argument("-c", "--config", help="Config file", required=True, type=str)
+    parser.add_argument(
+        "-od", "--output-dir", help="Output directory", required=True, type=str
+    )
+    parser.add_argument(
+        "-of",
+        "--output-filename",
+        help="Output ROOT filename",
+        default="AnalysisResults.root",
+        type=str,
+    )
+    # parser.add_argument("-n", "--nev", help="Number of events", default=10, type=int)
+    parser.add_argument("-v", "--verbose", help="be verbose", action="store_true")
+    args = parser.parse_args()
+    
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        "%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(funcName)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+
+    proc = PythiaOTFENC(
+        args.config, args.output_dir, args.output_filename, args.nev, args.verbose
+    )
+    proc.generate(args)
+
+
+if __name__ == "__main__":
+    main()
