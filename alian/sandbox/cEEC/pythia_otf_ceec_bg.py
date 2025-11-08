44438a83596ab1eea6f08fd2ae39983bfede16fb
Tucker Hwang <tucker_hwang@berkeley.edu>
2025-11-08 15:47:24 -0800

    Update background study

--- alian/sandbox/cEEC/pythia_otf_ceec_bg.py
+++ alian/sandbox/cEEC/pythia_otf_ceec_bg.py
@@ -5,23 +5,20 @@ import logging
 import os
 import sys
 import time
-from itertools import permutations
+# from itertools import permutations
 
 import numpy as np
 import ROOT
 import yaml
 
 import heppyy.util.fastjet_cppyy # noqa: F401
-# import heppyy.util.pythia8_cppyy
-# import heppyy.util.heppyy_cppyy
+import heppyy.util.pythia8_cppyy # noqa: F401
+import heppyy.util.heppyy_cppyy # noqa: F401
 
 from cppyy.gbl import fastjet as fj
 from cppyy.gbl.std import vector
 from heppyy.pythia_util import configuration as pyconf
-
-# to import more from heppyy: import the NAMESPACE declared in the .hh file
-# ex: for pythiafjext/pyfjtools.hh, we import:
-# from cppyy.gbl import pythiafjtools
+from cppyy.gbl import pythiafjtools
 
 logger = logging.getLogger(__name__)
 
@@ -31,14 +28,33 @@ def linbins(xmin, xmax, nbins):
 def logbins(xmin, xmax, nbins):
     return np.logspace(np.log10(xmin), np.log10(xmax), nbins + 1)
 
-def part_w_charge(pyp):
-    pj = fj.PseudoJet(pyp.px(), pyp.py(), pyp.pz(), pyp.e())
-    pj.set_user_index(int(pyp.charge()))
-    return pj
-def part_w_status(pyp):
-    pj = fj.PseudoJet(pyp.px(), pyp.py(), pyp.pz(), pyp.e())
-    pj.set_user_index(int(pyp.status()))
-    return pj
+def get_mother_status_list(pythia, p8p, mother_status_list):
+    mother_idxs = p8p.motherList()
+    if(len(mother_idxs) > 0):
+        for imother in mother_idxs:
+            get_mother_status_list(pythia, pythia.event[imother], mother_status_list)
+
+    mother_status_list.append( p8p.status() )
+    return mother_status_list
+
+def get_src(pythia, p8p):
+    mother_status_list = np.array(get_mother_status_list(pythia, p8p, []))
+    types = (mother_status_list/10).astype(int)
+    # 2 hard process
+    # 3 UE
+    # 4 ISR
+    # 5 parton shower
+    # 6 beam remnant
+    if -3 in types:
+        return 3
+    elif -2 in types:
+        return 2
+    elif -4 in types:
+        return 4
+    elif -6 in types:
+        return 6
+    else: # catchall for missing types
+        return 0
 
 class PythiaOTFENC(object):
     def __init__(self, config, odir, ofn, nev):
@@ -58,10 +74,10 @@ class PythiaOTFENC(object):
         self.jetR = config["jetR"] if "jetR" in config else 0.4
         self.eta_max = config["eta_max"]
         self.min_part_trk_pT = config["min_part_trk_pT"]
-        self.thr = config["thr"]
+        # self.thr = config["thr"]
         self.min_jet_pT = config["min_jet_pT"]
-        self.RL_min, self.RL_max, self.RL_nbins = config["RL_binning"]
-        self.pT_min, self.pT_max, self.pT_nbins = config["pT_binning"]
+        self.part_pT_min, self.part_pT_max, self.part_pT_nbins = config["part_pT_binning"]
+        self.jet_pT_min, self.jet_pT_max, self.jet_pT_nbins = config["jet_pT_binning"]
         self.shower = config["shower"]
         if self.shower not in ["dire", "vincia", "simple"]:
             logger.warning(
@@ -77,26 +93,29 @@ class PythiaOTFENC(object):
         self.scale_by_xsec = config.get('scale_by_xsec', True)
 
     def prepare(self):
-        self.RL_bins = logbins(self.RL_min, self.RL_max, self.RL_nbins)
-        self.pT_bins = linbins(self.pT_min, self.pT_max, self.pT_nbins)
+        self.part_pT_bins = logbins(self.part_pT_min, self.part_pT_max, self.part_pT_nbins)
+        self.jet_pT_bins = linbins(self.jet_pT_min, self.jet_pT_max, self.jet_pT_nbins)
 
         self.hists = {}
         self.hists['evw'] = ROOT.TH1F("evw", "evw;evw;cts", 100, -3.0, 3.0)
         self.hists['nev'] = ROOT.TH1F("hnev", "nev", 2, -0.5, 1.5)
-        self.hists['status'] = ROOT.TH1F("status", "status", 200, 0, 200)
 
-        for ptype in ["T", "Q", "P", "M", "PM"]:
-            self.hists[ptype] = ROOT.TH2D(
-                f"E2C_{ptype}_{self.thr}",
-                f"E2C_{ptype}_{self.thr};jet pt;R_{{L}}",
-                self.pT_nbins,
-                self.pT_bins,
-                self.RL_nbins,
-                self.RL_bins,
-            )
-        self.hists["jet_pT"] = ROOT.TH1D(
-            "jet_pT", "jet pt;pt gev;cts", self.pT_nbins, self.pT_bins
-        )
+        # 2 hard process
+        # 3 UE
+        # 4 ISR
+        # 5 parton shower
+        # 6 beam remnant
+        for src in [2, 3, 4, 6, 0]:
+            self.hists[src] = {}
+            for sgn in ["P", "M"]:
+                self.hists[src][sgn] = ROOT.TH2D(
+                    f"pt_{src}_{sgn}",
+                    f"pT_{src}_{sgn};part p_{{T}};jet p_{{T}}",
+                    self.part_pT_nbins,
+                    self.part_pT_bins,
+                    self.jet_pT_nbins,
+                    self.jet_pT_bins,
+                )
 
         self.jet_def = fj.JetDefinition(fj.antikt_algorithm, self.jetR)
 
@@ -105,7 +124,7 @@ class PythiaOTFENC(object):
         )
 
         self.part_pT_selector = fj.SelectorPtMin(self.min_part_trk_pT)
-        self.thr_selector = fj.SelectorPtMin(self.thr)
+        # self.thr_selector = fj.SelectorPtMin(self.thr)
 
         self.user_config = ['Init:showProcesses = off', 'Init:showChangedParticleData = on']
         if self.shower == "vincia":
@@ -213,26 +232,26 @@ class PythiaOTFENC(object):
         fj.ClusterSequence.print_banner()
 
         self.start = time.perf_counter()
-        pythia = pyconf.create_and_init_pythia_from_args(args, self.user_config)
+        self.pythia = pyconf.create_and_init_pythia_from_args(args, self.user_config)
 
-        if not pythia:
+        if not self.pythia:
             logger.fatal("Pythia initialization failed!")
             sys.exit(1)
         else:
             logger.info("Pythia configured and initialized.")
 
-        self.simulate(pythia)
+        self.simulate()
         logger.info(f"Simulation step completed in {time.perf_counter() - self.start:.2f} sec.")
 
-        self.finalize(pythia)
+        self.finalize()
         logger.info(f"Simulation completed in {time.perf_counter() - self.start:.2f} sec.")
 
-    def simulate(self, pythia):
+    def simulate(self):
         iev = 0
         # ping every 10% of total events
         ping = self.nev // 10
         while iev < self.nev:
-            if not pythia.next():
+            if not self.pythia.next():
                 self.nfailed += 1
                 continue
 
@@ -243,87 +262,86 @@ class PythiaOTFENC(object):
             #         if p.isFinal() and p.isCharged()
             #     ]
             # )
-            parts_ch = vector[fj.PseudoJet](
-                [part_w_charge(p) for p in pythia.event if p.isFinal() and p.isCharged()]
-            )
-            parts_sc = vector[fj.PseudoJet](
-                [part_w_status(p) for p in pythia.event if p.isFinal() and p.isCharged()]
-            )
+            # parts_ch = vector[fj.PseudoJet](
+            #     [part_w_charge(p) for p in pythia.event if p.isFinal() and p.isCharged()]
+            # )
+            # parts_sc = vector[fj.PseudoJet](
+            #     [part_w_status(p) for p in pythia.event if p.isFinal() and p.isCharged()]
+            # )
             # parts = vector[fj.PseudoJet](
             #     [part_w_charge(p) for p in pythia.event if p.isFinal() and p.isCharged()]
             # )
-            self.evw = pythia.info.weight()
-            self.analyze_event(parts_ch, parts_sc, pythia.info.pTHat())
+            # vector[int]
+            # print(type(int(pythiafjtools.Py8Part.kCharged)))
+            parts = pythiafjtools.vectorize_select(self.pythia, vector[int]([pythiafjtools.kCharged, pythiafjtools.kFinal]), 0, True, 0.13957) # assume pi+- mass
+            # parts = pythiafjtools.vectorize_select(self.pythia, [int(pythiafjtools.Py8Part.kCharged), int(pythiafjtools.Py8Part.kFinal)], 0, True, 0.13957) # assume pi+- mass
+            self.evw = self.pythia.info.weight()
+            self.pThat = self.pythia.info.pTHat()
+            self.analyze_event(parts)
 
             # Some "accepted" events don't survive hadronization step -- keep track here
             self.hists['nev'].Fill(0, self.evw)
             self.hists['evw'].Fill(self.evw)
 
-            # logger.info('NEW EVENT:')
-            # logger.info(f"Sum of weights (histogram): {self.hists['nev'].GetBinContent(1)}")
-            # logger.info(f"Sum of weights (Pythia): {pythia.info.weightSum()}")
             iev += 1
-            # if iev == 10:
-            #     break
             if iev % ping == 0:
                 logger.log(15, f"Completed {iev} events.")
 
-    def analyze_event(self, parts, parts_sc, pthat):
-        [self.hists["status"].Fill(part.user_index(), self.evw) for part in parts_sc]
+    def analyze_event(self, parts):
+        # [self.hists["status"].Fill(part.user_index(), self.evw) for part in parts_sc]
 
         jets = fj.sorted_by_pt(
             self.jet_selector(self.jet_def(self.part_pT_selector(parts)))
         )
         if self.reject_tail:
-            jets = [jet for jet in jets if jet.pt() < self.reject_tail * pthat]
+            jets = [jet for jet in jets if jet.pt() < self.reject_tail * self.pThat]
 
         for jet in jets:
-            self.analyze_jet(jet)
+            self.analyze_cones(parts, jet)
 
-    def analyze_jet(self, jet):
-        jet_pT = jet.perp()
-        self.hists["jet_pT"].Fill(jet_pT, self.evw)
+    def analyze_cones(self, parts, jet):
+        # coneR = np.sqrt(jet.area() / np.pi) if self.matched_cone else jetR
+        coneR = self.jetR
+        angle = np.pi / 2
+        rot_jet_1 = fj.PseudoJet()
+        rot_jet_1.reset_PtYPhiM(jet.pt(), jet.rap(), jet.phi() + angle, jet.m())
+        rot_jet_2 = fj.PseudoJet()
+        rot_jet_2.reset_PtYPhiM(jet.pt(), jet.rap(), jet.phi() - angle, jet.m())
+        for part in parts:
+            if np.sqrt((rot_jet_1.eta() - part.eta()) ** 2 + (rot_jet_1.delta_phi_to(part)) ** 2) <= coneR:
+                self.analyze_cone_part(part, "P", jet.pt())
+            # particles can only be in one cone or the other, so we can use elif
+            elif np.sqrt((rot_jet_2.eta() - part.eta()) ** 2 + (rot_jet_2.delta_phi_to(part)) ** 2) <= coneR:
+                self.analyze_cone_part(part, "M", jet.pt())
 
-        constituents = self.thr_selector(jet.constituents())
+    def analyze_cone_part(self, part, sgn, jet_pT):
+        p8part = pythiafjtools.getPythia8Particle(part)
+        src = get_src(self.pythia, p8part)
+        self.hists[src][sgn].Fill(part.pt(), jet_pT)
 
-        for p1, p2 in permutations(constituents, 2):
-            q1 = p1.user_index()
-            q2 = p2.user_index()
-            w = p1.perp() * p2.perp() / jet_pT / jet_pT
-            rL = np.sqrt(p1.delta_phi_to(p2) ** 2 + (p1.eta() - p2.eta()) ** 2)
-            if q1 > 0 and q2 > 0:
-                ptype = "P"
-            elif q1 < 0 and q2 < 0:
-                ptype = "M"
-            else:
-                ptype = "PM"
 
-            self.hists[ptype].Fill(jet_pT, rL, w * self.evw)
-            self.hists["T"].Fill(jet_pT, rL, w * self.evw)
-            self.hists["Q"].Fill(jet_pT, rL, w * q1 * q2 * self.evw)
-
-    def finalize(self, pythia):
-        pythia.stat()
+    def finalize(self):
+        self.pythia.stat()
         self.hists['nev'].SetBinError(1, 0)
 
         # scale_f = pythia.info.sigmaGen() / self.hists['nev'].GetBinContent(1)
         # Divide by the sum of weights to normalize histograms to a per event basis
         # then multiply by the cross-section to normalize by cross-section
-        scale_f = pythia.info.sigmaGen() / pythia.info.weightSum()
+        scale_f = self.pythia.info.sigmaGen() / self.pythia.info.weightSum()
 
-        logger.info(f"sigmaGen (Pythia) = {pythia.info.sigmaGen()} +- {pythia.info.sigmaErr()}")
+        logger.info(f"sigmaGen (Pythia) = {self.pythia.info.sigmaGen()} +- {self.pythia.info.sigmaErr()}")
         logger.info(f"Scaling factor (sigma/nev) = {scale_f} (with weightSum)")
-        logger.info(f"nTried: {pythia.info.nTried()}")
-        logger.info(f"nSelected: {pythia.info.nSelected()}")
-        logger.info(f"nAccepted: {pythia.info.nAccepted()}")
+        logger.info(f"nTried: {self.pythia.info.nTried()}")
+        logger.info(f"nSelected: {self.pythia.info.nSelected()}")
+        logger.info(f"nAccepted: {self.pythia.info.nAccepted()}")
         logger.info(f"Failed: {self.nfailed}")
         logger.info(f"Sum of weights (histogram): {self.hists['nev'].GetBinContent(1)}")
-        logger.info(f"Sum of weights (Pythia): {pythia.info.weightSum()}")
+        logger.info(f"Sum of weights (Pythia): {self.pythia.info.weightSum()}")
         logger.info(f"Number of successful events (histogram): {self.hists['nev'].GetEntries()}")
         # with open(f"{self.output_dir}/scales.txt", 'w') as f:
         #     f.write(f"{scale_f}")
         logger.info(
-            f"N total final events: {self.hists['nev'].GetBinContent(1)} with {pythia.info.nAccepted() - self.hists['nev'].GetBinContent(1)} events rejected."
+            f"N total final events: {self.hists['nev'].GetBinContent(1)} with {self.pythia.info.nAccepted() - self.hists['nev'].GetBinContent(1)} events rejected."
         )
 
         if self.scale_by_xsec:
@@ -331,16 +349,20 @@ class PythiaOTFENC(object):
         self._save_hists()
 
     def _scale_hists(self, scale_f):
-        for ptype in ["T", "Q", "P", "M", "PM"]:
-            self.hists[ptype].Scale(scale_f)
-        self.hists["jet_pT"].Scale(scale_f)
-        logger.info(f"Histograms scaled.")
+        for src in [2, 3, 4, 6, 0]:
+            for sgn in ["P", "M"]:
+                self.hists[src][sgn].Scale(scale_f)
+        # for ptype in ["T", "Q", "P", "M", "PM"]:
+        #     self.hists[ptype].Scale(scale_f)
+        # self.hists["jet_pT"].Scale(scale_f)
+        logger.info("Histograms scaled.")
 
     def _save_hists(self):
         with ROOT.TFile(self.output_path, "RECREATE") as f:
-            for hist in self.hists.values():
-                f.WriteTObject(hist)
-        logger.info(f"Histograms saved.")
+            for src in [2, 3, 4, 6, 0]:
+                for sgn in ["P", "M"]:
+                    f.WriteTObject(self.hists[src][sgn])
+        logger.info("Histograms saved.")
 
 
 def main():
